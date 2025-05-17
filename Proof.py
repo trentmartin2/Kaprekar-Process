72a07e23861e0c38c2407fed28b8aa1cd0056945
trentmartin2 <johnlikesrunescape@gmail.com>
2025-05-16 22:58:36 -0700

    Create Proof.py

---
+++ Proof.py
@@ -0,0 +1,30 @@
+final_output = [] # (output, iterations)
+failed_number = []
+num_dont_work = 0
+
+def machine(number):
+
+    n_list = []
+    for n in f'{number:04d}':
+        n_list.append(n)
+    s_n_list = sorted(n_list)
+    sorted_str = f'{s_n_list[0]}{s_n_list[1]}{s_n_list[2]}{s_n_list[3]}'
+    sorted_str_prime = f'{s_n_list[3]}{s_n_list[2]}{s_n_list[1]}{s_n_list[0]}'
+    sorted_number = int(sorted_str)
+    number_prime = int(sorted_str_prime)
+
+    return number_prime - sorted_number
+
+for i in range(1000, 10000):
+    n = i
+    iterations = 0
+    while n != 6174 and iterations < 100:
+        n = machine(n)
+        iterations += 1
+    final_output.append([n, iterations])
+
+for i in final_output:
+    if i[0] != 6174:
+        num_dont_work += 1
+
+print(num_dont_work)
